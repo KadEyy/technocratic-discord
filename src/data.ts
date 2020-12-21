18870fc27487ca87cd02520af857078f9e6147be
Jakub Mizera <k.cstmz@gmail.com>
2020-12-21 21:16:28 +0100

    forgot to save all

--- src/data.ts
+++ src/data.ts
@@ -1,4 +1,4 @@
-export const token: string = 'NzkwMjk3Mjc1NDUyMjI3NjI0.X9-jsQ.58XbeYdOrB4L2n5Z9GBob4wnncA';
+export const token: string = process.env.TOKEN;
 export const prefix: string = '!';
 export const guestRoleID: string = '790304389826084887';
 export const welcomeChannelID: string = '790303718872055889';
